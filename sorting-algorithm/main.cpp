582010eb3c770a6cd30e3a9654e50e0d2c04c80f
Felipe Teles <felipersteles@gmail.com>
2024-04-30 13:34:43 -0300

    Initial Commit

---
+++ sorting-algorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sorting-algorithm
+//
+//  Created by Felipe Teles on 30/04/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
